996cd0b5cf6cdc8c4b8f000122e7d2a0a5ff941f
eduard dedu <eduardcdedu@gmail.com>
2016-09-12 21:54:58 +0200

    Deployment context changed to context root "/"

--- src/main/java/tools/ResourcesBean.java
+++ src/main/java/tools/ResourcesBean.java
@@ -14,7 +14,7 @@ public class ResourcesBean implements Serializable {
     private String clientName = "smartloans-test";
     private String apiKey = "6e907d69e7acbbdf8411b62010070c0a6309bebbe5ca549136ca6e067e2543a4";
     private String ownerExternalId = "100780";
-    private final String contextRoot = "K-tester";
+    private final String appName = "K-tester";
 
 
     public String getClientName() {
@@ -35,7 +35,7 @@ public class ResourcesBean implements Serializable {
         return ownerExternalId;
     }
 
-    public String getContextRoot() { return contextRoot; }
+    public String getAppName() { return appName; }
 
 
 }
--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -2,7 +2,7 @@
  * Created by eduarddedu on 21/06/16
  */
 function redirect(page) {
-    var location = window.location.origin + '/K-tester/' + page;
+    var location = window.location.origin + page;
     window.location.replace(location);
 }
 
