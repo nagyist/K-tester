aff2db47bcae1c404091716bfed573120e0aaa99
eduard dedu <eduardcdedu@gmail.com>
2016-09-27 23:21:54 +0200

    Update kontomatik.js - 4 spaces indentation

--- src/main/webapp/js/kontomatik.js
+++ src/main/webapp/js/kontomatik.js
@@ -1,36 +1,34 @@
-var id = Math.random().toString(36).substring(2,7);
-
 embedKontomatik({
-  client: window.clientName,
-  divId: 'kontomatik',
-  locale: 'en',
-  showFavicons: true,
-  ownerExternalId: id,
-  onSuccess: function (target, sessionId, sessionIdSignature) {
-    // Pass session params to the backend
-    $.ajax({
-      type: 'POST',
-      url: window.location.origin + '/session/data',
-      data:
-      {
-        sessionId: sessionId,
-        sessionIdSignature: sessionIdSignature,
-        target: target,
-        ownerId: id
-      },
-      dataType: 'text',
-      success: function (data, textStatus, jqXHR) {
-        redirect('/commands.xhtml');
+    client: window.clientName,
+    divId: 'kontomatik',
+    locale: 'en',
+    showFavicons: true,
+    ownerExternalId: Math.random().toString(36).substring(2,7),
+    onSuccess: function (target, sessionId, sessionIdSignature) {
+        // Pass session params to the backend
+        $.ajax({
+            type: 'POST',
+            url: window.location.origin + '/session/data',
+            data:
+            {
+                sessionId: sessionId,
+                sessionIdSignature: sessionIdSignature,
+                target: target,
+                ownerId: id
+            },
+            dataType: 'text',
+            success: function (data, textStatus, jqXHR) {
+                redirect('/commands.xhtml');
 
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        alert(errorThrown);
-      }
-    });
-  },
-  onError: function (exception) {
-    alert('Error: ' + exception);
-  }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    },
+    onError: function (exception) {
+        alert('Error: ' + exception);
+    }
 });
 
 
